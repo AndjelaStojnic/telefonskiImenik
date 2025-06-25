f763dd9c8c6aec420ae720e46eeafadc415ceb08
Andjela <andjelastojnic1607@gmail.com>
2025-06-25 13:44:53 +0200

    dashboard fix (logout)

--- frontend/client/src/App.tsx
+++ frontend/client/src/App.tsx
@@ -1,18 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Login";
-import Register from "./components/Register";
+import Register from "./Register";
 import Dashboard from "./Dashboard";
 import VerifyEmail from "./VerifyEmail";
 
-
 export default function App() {
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    window.location.href = "/";
+  };
+
   return (
     <Router>
       <div style={{ fontFamily: "sans-serif" }}>
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard onLogout={handleLogout} />} />
           <Route path="/verify-email" element={<VerifyEmail />} />
         </Routes>
       </div>
